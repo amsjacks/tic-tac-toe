e95bdd3d442429713fc06142585dfc489f1a039c
amsjacks <aislin.ms.jackson@gmail.com>
2017-01-01 23:43:50 -0800

    Everything that I wanted to move from the main app.py file over to game.py has been moved, although I still need to implement some error handling.

--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-def run_game(game_count, p1, p2):
-    # Initialize a bunch of game-specific vars
-    clear_board()
-    x_player = p1
-    o_player = p2
-    turn_count = 0
-    if not game_count % 2 == 0:
-        # Turn taking for successive games
-        x_player = p2
-        o_player = p1
-
-    show_board()
-    while (not win("X")) and (not win("O") and (not is_tied())):
-        if turn_count % 2 == 0:
-            square = input("{}, please enter the code for where you would like to place your X (e.g., A1, C2): "
-                           .format(x_player))
-            # TODO: Add exception if input is not a valid square
-            move("X", square)  # TODO: put the symbol in a variable so that can be customized by player
-        else:
-            square = input("{}, please enter the code for where you would like to place your O (e.g., A1, C2): "
-                           .format(o_player))
-            # TODO: Add exception if input is not a valid square
-            move("O", square)
-        turn_count += 1
-        show_board()
 
     if is_tied():
         print("The game is tied!")
@@ -37,51 +12,6 @@ def run_game(game_count, p1, p2):
     else:
         print("Goodbye!")
 
-
-def show_board():
-    print("      A     B     C  ")
-    print("   |_____|_____|_____")
-    print("   |     |     |     ")
-    print(" 1 |  {}  |  {}  |  {}  ".format(board["A1"], board["B1"], board["C1"]))
-    print("   |_____|_____|_____")
-    print("   |     |     |     ")
-    print(" 2 |  {}  |  {}  |  {}  ".format(board["A2"], board["B2"], board["C2"]))
-    print("   |_____|_____|_____")
-    print("   |     |     |     ")
-    print(" 3 |  {}  |  {}  |  {}  ".format(board["A3"], board["B3"], board["C3"]))
-    print("   |_____|_____|_____")
-
-
-def clear_board():
-    global board
-    board = {"A1": " ", "A2": " ", "A3": " ",
-            "B1": " ", "B2": " ", "B3": " ",
-            "C1": " ", "C2": " ", "C3": " "}
-
-def move(symbol, square):
-    global board
-    # TODO: throw exception if the square is already occupied
-    board[square] = symbol
-
-# Tests whether the player marking with a particular symbol has won
-def win(symbol):
-    symb_win = False
-    if board["A1"] == symbol:
-        symb_win = (board["A2"] == symbol and board["A3"] == symbol) or (
-            board["B2"] == symbol and board["C3"] == symbol) or (board["B1"] == symbol and board["C1"] == symbol)
-    elif board["B2"] == symbol:
-        symb_win = (board["B1"] == symbol and board["B3"] == symbol) or (
-            board["A2"] == symbol and board["C2"] == symbol) or (board["C1"] == symbol and board["A3"] == symbol)
-    elif board["C3"] == symbol:
-        symb_win = (board["A3"] == symbol and board["B3"] == symbol) or (board["C1"] == symbol and board["C2"] == symbol)
-    return symb_win
-
-def is_tied():
-    return (not " " in board.values()) and (not win("X") and (not win("O")))
-
-
-# Set up global value
-board = {}
 
 # Prompt users for their names
 print("Welcome to Tic Tac Toe!")
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from board import Board
 
 class Game(object):
+
     def __init__(self,p1,p2,game_count=1,x_symb="X",y_symb="y"):
         # Turn taking for successive games
         if not game_count %2 == 0:
@@ -14,5 +15,43 @@ class Game(object):
         self.turn_count = 0
         self.board = Board(self.x_symb,self.y_symb)
 
-    def run_game(self,game_count):
+
+    def run_game(self):
         self.board.show_board()
+        while (not self.win(self.x_symb)) and (not self.win(self.y_symb) and (not self.is_tied())):
+            if self.turn_count % 2 == 0:
+                square = input("{}, please enter the code for where you would like to place your {} (e.g., A1, C2): "
+                               .format(self.x_player,self.x_symb))
+                # TODO: Add exception if input is not a valid square
+                self.board.move(self.x_symb, square)
+            else:
+                square = input("{}, please enter the code for where you would like to place your {} (e.g., A1, C2): "
+                               .format(self.y_player,self.y_symb))
+                # TODO: Add exception if input is not a valid square
+                self.board.move(self.y_symb, square)
+            self.turn_count += 1
+            self.board.show_board()
+        if self.is_tied():
+            print("The game is tied!")
+        elif self.win(self.x_symb):
+            print("Congratulations, {}! You have won!".format(self.x_player))
+        else:
+            print("Congratulations, {}! You have won!".format(self.y_player))
+
+    # Tests whether the player marking with a particular symbol has won
+    def win(self,symbol):
+        symb_win = False
+        if self.board["A1"] == symbol:
+            self.board = (self.board["A2"] == symbol and self.board["A3"] == symbol) or (
+                self.board["B2"] == symbol and self.board["C3"] == symbol) or (self.board["B1"] == symbol and self.board["C1"] == symbol)
+        elif self.board["B2"] == symbol:
+            symb_win = (self.board["B1"] == symbol and self.board["B3"] == symbol) or (
+                self.board["A2"] == symbol and self.board["C2"] == symbol) or (self.board["C1"] == symbol and self.board["A3"] == symbol)
+        elif self.board["C3"] == symbol:
+            symb_win = (self.board["A3"] == symbol and self.board["B3"] == symbol) or (
+                self.board["C1"] == symbol and self.board["C2"] == symbol)
+        return symb_win
+
+    # Tests whether the game is tied
+    def is_tied(self):
+        return (not " " in self.board.values()) and (not self.win("X") and (not self.win("O")))
